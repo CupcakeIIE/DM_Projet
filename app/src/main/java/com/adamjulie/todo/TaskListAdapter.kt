d0cf257230cf8eb890f1c4fd9680b21b914249f6
Adam <62399795+Adamme-le-codeur-fou@users.noreply.github.com>
2024-11-28 16:33:27 +0100

    item view

--- app/src/main/java/com/adamjulie/todo/TaskListAdapter.kt
+++ app/src/main/java/com/adamjulie/todo/TaskListAdapter.kt
@@ -1,13 +1,37 @@
 package com.adamjulie.todo
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
 class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
     var currentList: List<String> = emptyList()
 
     // on utilise `inner` ici afin d'avoir accès aux propriétés de l'adapter directement
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        // on utilise `view` ici afin d'avoir accès à la vue de l'item
+        private val view = itemView
+        private val textView = view.findViewById<TextView>(R.id.task_title)
+
         fun bind(taskTitle: String) {
-            // on affichera les données ici
+            textView.text = taskTitle
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(currentList[position])
     }
 }
