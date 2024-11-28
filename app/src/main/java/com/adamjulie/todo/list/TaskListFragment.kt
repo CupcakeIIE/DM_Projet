11b642a90e96926c64238f31204378124e50ad64
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2024-11-28 15:04:17 +0100

    TaskList

--- app/src/main/java/com/adamjulie/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/adamjulie/todo/list/TaskListFragment.kt
@@ -1,3 +1,22 @@
 package com.adamjulie.todo.list
 
-class TaskListFragment : Fragment() {}
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.adamjulie.todo.R
+
+class TaskListFragment : Fragment() {
+
+    private var taskList = listOf("Task 1", "Task 2", "Task 3")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_blank, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
