3f3430ccad8d1b089fdfc49a150a8e1b544e7e0e
Adam <62399795+Adamme-le-codeur-fou@users.noreply.github.com>
2024-11-28 16:01:21 +0100

    taskListAdapter et recyclerview

--- app/src/main/java/com/adamjulie/todo/list/TaskListFragment.kt
+++ app/src/main/java/com/adamjulie/todo/list/TaskListFragment.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.adamjulie.todo.R
+import com.adamjulie.todo.TaskListAdapter
 
 class TaskListFragment : Fragment() {
 
     private var taskList = listOf("Task 1", "Task 2", "Task 3")
+    private val adapter = TaskListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.recycler_view)
+        adapter.currentList = taskList
         val rootView = inflater.inflate(R.layout.fragment_blank, container, false)
         return super.onCreateView(inflater, container, savedInstanceState)
     }
