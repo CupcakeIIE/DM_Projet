27e0a77a9aa122a6b4979ea98a297cac3e0f14b1
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2024-12-26 12:11:47 +0100

    slide 6 tp3

--- app/src/main/java/com/adamjulie/todo/data/Api.kt
+++ app/src/main/java/com/adamjulie/todo/data/Api.kt
@@ -36,4 +36,9 @@ object Api {
             .addConverterFactory(jsonSerializer.asConverterFactory("application/json".toMediaType()))
             .build()
     }
+
+
+    val userWebService : UserWebService by lazy {
+        retrofit.create(UserWebService::class.java)
+    }
 }
