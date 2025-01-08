14283a7a1cdaa42c93db2af85820e12e42108bf5
CupcakeIIE <122415151+CupcakeIIE@users.noreply.github.com>
2025-01-08 14:27:25 +0100

    tp4 slde 4 enfiiiiiiiiiiin

--- app/src/main/java/com/adamjulie/todo/data/UserWebService.kt
+++ app/src/main/java/com/adamjulie/todo/data/UserWebService.kt
@@ -1,11 +1,14 @@
 package com.adamjulie.todo.data
 
+import android.graphics.Bitmap
 import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import java.io.File
 
 interface UserWebService {
     @GET("/sync/v9/user/")
@@ -14,5 +17,17 @@ interface UserWebService {
     @Multipart
     @POST("sync/v9/update_avatar")
     suspend fun updateAvatar(@Part avatar: MultipartBody.Part): Response<User>
+
+    private fun Bitmap.toRequestBody(): MultipartBody.Part {
+        val tmpFile = File.createTempFile("avatar", "jpg")
+        tmpFile.outputStream().use { // *use*: open et close automatiquement
+            this.compress(Bitmap.CompressFormat.JPEG, 100, it) // *this* est le bitmap ici
+        }
+        return MultipartBody.Part.createFormData(
+            name = "avatar",
+            filename = "avatar.jpg",
+            body = tmpFile.readBytes().toRequestBody()
+        )
+    }
 }
 
